f6ae1e0c091f7e400f47b8a47585918b1549ab30
Gabriel Tobias <gabriel.tobias@relateiq.com>
2016-02-23 16:58:30 -0800

    refactoring ex 1_2 - use PrintF instead of printLn plus SprintF

--- chapter1/exercise_1_2.go
+++ chapter1/exercise_1_2.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
     for i := 1; i < len(os.Args); i++ {
-        fmt.Println(fmt.Sprintf("%v - %s", i, os.Args[i]))
+        fmt.Printf("%v - %s\n", i, os.Args[i])
     }
 }
